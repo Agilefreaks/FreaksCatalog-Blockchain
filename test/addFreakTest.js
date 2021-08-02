3ff2d1183d8df3603e874e3ef54dd23eadd5e137
craciungab12 <craciungabi12@yahoo.com>
2021-08-02 14:16:11 +0300

    Delete a freak + tests

--- test/addFreakTest.js
+++ test/addFreakTest.js
@@ -1,88 +1,54 @@
 const { expect, assert } = require("chai");
 const EventEmitter = require('events');
 
-describe("FreakTeam", function () {
-  it("AddFreak fails when it's not called by hr", async function () {
-
-
-    const addFreak = await ethers.getContractFactory("FreakTeam");
-    const [admin, hr, other, financial] = await ethers.getSigners();
-    const freak = await addFreak.deploy(hr.address, financial.address);
-    await expect(freak.connect(other).addNewFreak(other.address,"TestFreak",2021,1,1,1)).to.be.revertedWith('Caller is not a hr')
-
+describe("addFreakTests", function () {
+  beforeEach(async() => {
+    addFreak = await ethers.getContractFactory("FreakTeam");
+    [admin, hr, other, financial] = await ethers.getSigners();
+    freak = await addFreak.deploy(hr.address, financial.address);
   });
-  it("AddFreak fails when the name is not added", async function () {
-    const addFreak = await ethers.getContractFactory("FreakTeam");
-    const [admin, hr, other, financial] = await ethers.getSigners();
-    const freak = await addFreak.deploy(hr.address, financial.address);
 
+  it("AddFreak fails when it's not called by hr", async function () {
+    await expect(freak.connect(other).addNewFreak(other.address,"TestFreak",2021,1,1,1)).to.be.revertedWith('Caller is not a hr')
+  });
+
+  it("AddFreak fails when the name is not added", async function () {
     await freak.connect(hr).addNewFreak(other.address,"TestFreak",2021,1,1,1);
     expect((await freak.freaks(other.address)).name).to.equal("TestFreak");
   });
-  it("AddFreak fails when the startDate is not added", async function () {
-    const addFreak = await ethers.getContractFactory("FreakTeam");
-    const [admin, hr, other, financial] = await ethers.getSigners();
-    const freak = await addFreak.deploy(hr.address, financial.address);
 
+  it("AddFreak fails when the startDate is not added", async function () {
     await freak.connect(hr).addNewFreak(other.address,"TestFreak",2021,1,1,1);
     expect((await freak.freaks(other.address)).startDate).to.equal(2021);
-
   });
-  it("AddFreak fails when employeeNumber is not added", async function () {
-    const addFreak = await ethers.getContractFactory("FreakTeam");
-    const [admin, hr, other, financial] = await ethers.getSigners();
-    const freak = await addFreak.deploy(hr.address, financial.address);
 
+  it("AddFreak fails when employeeNumber is not added", async function () {
     await freak.connect(hr).addNewFreak(other.address,"TestFreak",2021,1,1,1);
     expect((await freak.freaks(other.address)).employeeNumber).to.equal(1);
-  
   });
+
   it("AddFreak fails when role is not added", async function () {
-    const addFreak = await ethers.getContractFactory("FreakTeam");
-    const [admin, hr, other, financial] = await ethers.getSigners();
-    const freak = await addFreak.deploy(hr.address, financial.address);
-
     await freak.connect(hr).addNewFreak(other.address,"TestFreak",2021,1,1,1);
-    expect((await freak.freaks(other.address)).choiceRole).to.equal(1);
-
+    expect((await freak.freaks(other.address)).choiceRole).to.equal(1)
   });
-  it("AddFreak fails when skill is not added", async function () {
-    const addFreak = await ethers.getContractFactory("FreakTeam");
-    const [admin, hr, other, financial] = await ethers.getSigners();
-    const freak = await addFreak.deploy(hr.address, financial.address);
 
+  it("AddFreak fails when skill is not added", async function () {
     await freak.connect(hr).addNewFreak(other.address,"TestFreak",2021,1,1,1);
     expect((await freak.freaks(other.address)).choiceSkill).to.equal(1);
-
   });
+
   it("AddFreak fails if on the address already exists one freak", async function () {
-    const addFreak = await ethers.getContractFactory("FreakTeam");
-    const [admin, hr, other, financial] = await ethers.getSigners();
-    const freak = await addFreak.deploy(hr.address, financial.address);
-
     expect((await freak.freaks(other.address)).employeeNumber).to.equal(0);
-
   });
-  it("AddFreak fails when mint doesn't modify Freak balance", async function () {
-    const addFreak = await ethers.getContractFactory("FreakTeam");
-    const [admin, hr, other, financial] = await ethers.getSigners();
-    const freak = await addFreak.deploy(hr.address, financial.address);
 
+  it("AddFreak fails when mint doesn't modify Freak balance", async function () {
     await freak.connect(hr).addNewFreak(other.address,"TestFreak",2021,1,1,1);
     expect(await freak.balanceOf(other.address, (await freak.freaks(other.address)).employeeNumber)).to.not.equal(0);
-
   });
-  it("AddFreak fails when it doesn t emit an event", async function () {
-    const addFreak = await ethers.getContractFactory("FreakTeam");
-    const [admin, hr, other, financial] = await ethers.getSigners();
-    const freak = await addFreak.deploy(hr.address, financial.address);
 
+  it("AddFreak fails when it doesn t emit an event", async function () {
     await(expect( freak.connect(hr).addNewFreak(other.address,"TestFreak",2021,1,1,1)).to.emit( 
             freak, "addedFreak").withArgs(
               other.address,"TestFreak",2021,1,1,1));
-
-
-
-
   });
-}) 
+});
