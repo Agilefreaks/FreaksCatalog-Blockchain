2c40740dec77d0b17659d387cae8262ce53299fe
craciungabi12 <craciungabi12@yahoo.com>
2021-08-30 15:48:13 +0300

    Functions for: freakScore + getShare +  getFreaks  and added Norm for all freaks

---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-solhint");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+module.exports = {
+  solidity: {
+    version: "0.8.7",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
